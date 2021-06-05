f96e57d9de208e573bd045cc9186af4263109f66
TinyWeakling <On.Lapol@mail.com>
2021-06-05 20:46:45 +0800

    update

---
+++ RemoveFinalSlides.bat
@@ -0,0 +1,7 @@
+del FinalSlides.log
+del FinalSlides.aux
+del FinalSlides.nav
+del FinalSlides.out
+del FinalSlides.snm
+del FinalSlides.synctex*
+del FinalSlides.toc
---
+++ makefile.bat
@@ -0,0 +1,13 @@
+@echo off
+
+xelatex XinHuang.tex
+
+bibtex xelatex LeyuanPan.bib
+
+bibtex xelatex LeyuanPan.bib
+
+xelatex XinHuang.tex
+
+xelatex XinHuang.tex
+
+@echo on
